5663e8a26c16575bc81216072ae6108b0f9629ae
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-01-18 12:49:12 -0800

    Started playing with react inline styling

--- client/js/components/QueuedVote.react.js
+++ client/js/components/QueuedVote.react.js
@@ -19,17 +19,38 @@ var QueuedVote = React.createClass({
 
   render: function() {
     var voteType = this.props.voteType;
+
+    var upVoteStyle = {
+      color: 'white',
+      backgroundColor: "#20be22",
+      backgroundImage: 'img/arrow.png',
+      borderStyle: voteType == VoteTypes.UPVOTED ? 'solid' : 'none',
+      borderColor: "#285328",
+      borderWidth: 5,
+    }
+
+    var downVoteStyle = {
+      color: 'white',
+      backgroundColor: "#d52626",
+      backgroundImage: 'img/arrow.png',
+      borderStyle: voteType == VoteTypes.DOWNVOTED ? 'solid' : 'none',
+      borderColor: "#5e1919",
+      borderWidth: 5,
+    }
+
     return (
       <div className="queuedVoteButtons">
         <button
           className={"btn-upvote" + (voteType == VoteTypes.UPVOTED ? "-selected" : "")}
-          onClick={voteType == VoteTypes.UPVOTED ? this.handleCancel : this.handleUpvote} >
-          {(voteType == VoteTypes.UPVOTED ? "cancel" : "") + " upvote"}
+          onClick={voteType == VoteTypes.UPVOTED ? this.handleCancel : this.handleUpvote}
+          style={upVoteStyle} >
+          upvote
         </button>
         <button
           className={"btn-downvote"  + (voteType == VoteTypes.DOWNVOTED ? "-selected" : "")}
-          onClick={voteType == VoteTypes.DOWNVOTED ? this.handleCancel : this.handleDownvote} >
-          {(voteType == VoteTypes.DOWNVOTED ? "cancel" : "") + " downvote"}
+          onClick={voteType == VoteTypes.DOWNVOTED ? this.handleCancel : this.handleDownvote}
+          style={downVoteStyle} >
+          downvote
         </button>
       </div>
     );
