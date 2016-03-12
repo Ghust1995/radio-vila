db870d1f6c3ce204c303c03e38790275c26526e0
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-03-12 01:47:17 -0300

    player - PLEASE READ THE FILE I ADDED

--- player-client/js/utils/PlayerWebAPIUtils.js
+++ player-client/js/utils/PlayerWebAPIUtils.js
@@ -5,6 +5,12 @@ var i = 0;
 
 module.exports = {
   requestSong: function() {
+
+    // TODO: make calls to real API
+    // DELETE .../songs/{currentSongID}
+    // GET    .../songs[0]
+    // POST   .../currentSong  song
+
     var songs = ['https://www.youtube.com/watch?v=a1Y73sPHKxw', 'https://www.youtube.com/watch?v=2D8oUY6bkdA', "https://www.youtube.com/watch?v=4zLfCnGVeL4", 'https://www.youtube.com/watch?v=oUFJJNQGwhk'];
     PlayerServerActionCreators.requestSongSuccess(songs[i]);
     i = (i + 1) % songs.length;
